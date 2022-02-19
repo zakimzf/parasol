b71af2e2ba7b76fc152c32121100faae4b7087d2
Clint.Network <me@clint.network>
2022-02-19 14:57:05 +0200

    Create privacy-policy page

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,16 @@
+import Heading from "../components/utils/heading";
+import Markdown from 'markdown-to-jsx';
+
+import PRIVACY_POLICY from '../content/privacy-policy.md';
+
+export default () =>
+    <>
+        <Heading tagline={"Legal"} title={"Privacy Policy"}
+                 description={"How we collect and process your information."}/>
+        <section>
+            <div className={"mx-auto max-w-3xl prose prose-lg prose-invert"}>
+                <Markdown>{PRIVACY_POLICY}</Markdown>
+            </div>
+        </section>
+    </>
+
