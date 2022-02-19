baac09ec543bd70f967ae572f799a60adbc63a4d
Clint.Network <me@clint.network>
2022-02-19 23:07:17 +0200

    Rename and move `wallet-connector`

---
+++ src/components/wallet-connector/WalletModalProvider.tsx
@@ -0,0 +1,15 @@
+import React, {FC, useState} from 'react';
+import {WalletModalContext} from './useWalletModal';
+import {WalletModal} from './WalletModal';
+
+export const WalletModalProvider: FC = ({children}) => {
+    const [visible, setVisible] = useState(false);
+    return <WalletModalContext.Provider
+        value={{
+            visible,
+            setVisible,
+        }}>
+        {children}
+        <WalletModal/>
+    </WalletModalContext.Provider>
+};
