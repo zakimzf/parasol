7c2cc5126f1871129cc33d76c9239ff5b1b7a436
Clint.Network <me@clint.network>
2022-02-19 23:06:04 +0200

    Add `TokenChooser` component

---
+++ src/components/TokenChooser/useTokenModal.tsx
@@ -0,0 +1,24 @@
+import {createContext, useContext} from 'react';
+import {PublicKey} from "@solana/web3.js";
+
+export enum TokenChooserMode {
+	Input,
+	Output
+}
+
+export interface TokenChooserContextState {
+	visible: boolean;
+	setVisible: (open: boolean) => void;
+	mode: TokenChooserMode;
+	setMode: (open: TokenChooserMode) => void;
+	input: PublicKey;
+	output: PublicKey;
+	setInput: (token: PublicKey) => void;
+	setOutput: (token: PublicKey) => void;
+}
+
+export const TokenChooserContext = createContext<TokenChooserContextState>({} as TokenChooserContextState);
+
+export function useTokenModal(): TokenChooserContextState {
+	return useContext(TokenChooserContext);
+}
