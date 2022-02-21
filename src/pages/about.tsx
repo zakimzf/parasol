ed290050391d9948b88b480d1f2310ed3acd77ba
Tom <tom@parasol.finance>
2022-02-21 16:03:16 +0700

    Remove period

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -18,7 +18,7 @@ const About = () =>
                             </span>
                             <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl">
                                 The <span className={"text-purple-2"}>future</span> is already here with <span
-                                className="text-purple-2">Parasol</span>.
+                                className="text-purple-2">Parasol</span>
                             </span>
                         </h1>
                         <p className="mt-3 text-base sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
@@ -45,8 +45,7 @@ const About = () =>
                             </Link>
                         </div>
                     </div>
-                    <div
-                        className="mt-12  relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+                    <div className="mt-12  relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
                         <div className="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
                             <button type="button"
                                     className=" block w-full bg-white rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
