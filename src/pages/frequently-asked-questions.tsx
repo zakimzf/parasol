d784ac57f67a6fbe005518fd6f2da12b8c7f39ae
Clint.Network <me@clint.network>
2022-02-21 12:49:16 +0200

    Create FAQ (#3)
    * Update FAQ
    
    * Fix JSON
    
    * Add button for read FAQ in doc
    
    Co-authored-by: Alex <33532075+litasio@users.noreply.github.com>

--- src/pages/frequently-asked-questions.tsx
+++ src/pages/frequently-asked-questions.tsx
@@ -1,14 +1,54 @@
 import {Disclosure} from '@headlessui/react'
-import {ChevronDownIcon} from '@heroicons/react/outline'
+import {ChevronDoubleRightIcon, ChevronDownIcon} from '@heroicons/react/outline'
 import Heading from "../components/utils/heading";
+import Container from "../components/container";
 
 const faqs = [
     {
-        question: "What's the best thing about Switzerland?",
+        question: "What Is Parasol Finance?",
         answer:
-            "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+            "Parasol Finance is the first-ever community-governed IDO ecosystem that is built on Solana with the needs of both investors and projects alike."
     },
-    // More questions...
+    {
+        question: "What Is Staking?",
+        answer:
+            "Staking is the act of securing cryptocurrency assets on a platform or network for a reward or annualized returns that are paid out regularly."
+    },
+    {
+        question: "What is Yield Farming?",
+        answer:
+            "Yield Farming in the cryptocurrency industry is earning an interest on your cryptocurrency investment. The initial investment value grows over the time that the asset is staked or landed out."
+    },
+    {
+        question: "What Is Decentralized Exchange (DEX)?",
+        answer:
+            "A decentralized exchange or DEX is a financial services platform used for trading, buying, and selling digital assets. On a DEX, users are granted the chance to transact peer-to-peer and directly on the blockchain without a centralized intermediary. Decentralized exchanges are often democratically managed with decentralized governance organizations, however, they do not serve as custodians of users' funds. In the absence of a central authority charging fees for services, DEXs are cheaper than their centralized counterparts."
+    },
+    {
+        question: "What is an Initial Dex Offering (IDO)?",
+        answer:
+            "An IDO is an approach to crowdfunding funds for new cryptocurrency projects. It means the project is launching their native coins or tokens through a Decentralized Exchange to be used within the ecosystem and the protocol. It also gives the protocol funds to further develop and pay for business expenses.",
+    },
+    {
+        question: "What is Decentralized Autonomous Exchange (DAO)?",
+        answer:
+            "A DAO is a community-led entity that has no central authority. It is fully transparent and autonomous. The business rules and activities are decided by the holders of the given governance token, usually through voting processes."
+    },
+    {
+        question: "What Is an Airdrop?",
+        answer:
+            "Airdrop is referred to as a token distribution method whereby assets are directly transmitted to user wallets without any charges. Airdrops are also used to raise awareness around a project or as part of a fundraising mechanism."
+    },
+    {
+        question: "What Is an NFT?",
+        answer:
+            "Non-fungible tokens, or NFTs, are the latest cryptocurrency phenomenon to go mainstream in the industry. Non-fungible tokens, or NFTs, are pieces of digital content directly stored to the blockchain."
+    },
+    {
+        question: "Who runs Parasol Finance?",
+        answer:
+            "Presently, decisions about Parasol Finance are taken by the core team, however we expect to turn this into a DAO-governed model as soon as possible."
+    }
 ]
 
 export default () =>
@@ -40,5 +80,16 @@ export default () =>
                     </Disclosure>)}
                 </dl>
             </div>
+        </section>
+        <section>
+            <Container>
+                <div className={"flex justify-center pt-12"}>
+                    <a href={"https://docs.parasol.finance/parasol-ecosystem/frequently-asked-questions"}
+                       className={`flex items-center justify-center items-center gap-x-1 g-purple-2 text-white bg-transparent border border-white px-7 py-3 text-base font-medium rounded-md`}>
+                        See The Full FAQ
+                        <ChevronDoubleRightIcon className={"w-4"}/>
+                    </a>
+                </div>
+            </Container>
         </section>
     </>
