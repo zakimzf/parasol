5a3ea835a490b7ea720600c74105da8bbc35a928
soft-top-notch <soft.dev.96@gmail.com>
2022-02-24 08:21:57 -0800

    Updated some styles using ESLint and implemented some functionalities in the swap page

--- src/components/token-chooser/TokenModal.tsx
+++ src/components/token-chooser/TokenModal.tsx
@@ -1,130 +1,159 @@
-import React, {FC, Fragment, useEffect, useState} from 'react';
-import {Dialog, Transition} from "@headlessui/react";
-import {TokenChooserMode, useTokenModal} from "./useTokenModal";
-import {TOKEN_LIST_URL} from "@jup-ag/core";
-import {SearchIcon} from "@heroicons/react/outline";
-import {PublicKey} from "@solana/web3.js";
-import {Token} from "./constants";
+import React, { FC, Fragment, useEffect, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { TokenChooserMode, useTokenModal } from "./useTokenModal";
+import { TOKEN_LIST_URL } from "@jup-ag/core";
+import { SearchIcon } from "@heroicons/react/outline";
+import { PublicKey } from "@solana/web3.js";
+import { Token } from "./constants";
 
 export const TokenModal: FC = () => {
-    const {visible, setVisible, mode, setInput, setOutput} = useTokenModal();
-    const [value, setValue] = useState('')
-    const [tokens, setTokens] = useState<Token[]>([])
-    useEffect(() => {
-        fetch(TOKEN_LIST_URL["mainnet-beta"])
-            .then(response => response.json())
-            .then(result => setTokens(result))
-    }, [])
-    const filteredTokens = tokens
-        .filter(token => {
-            if (!value) return true
-            if (token.name.toLowerCase().includes(value.toLowerCase())
-                || token.symbol.toLowerCase().includes(value.toLowerCase())
-                || token.address.includes(value)) return true
-        });
-    const close = () => {
-        setVisible(false)
-        setValue('')
-    };
-    let featuredTokens = [
-        {
-            name: 'SOL',
-            icon: tokens.find(x => x.symbol === "SOL")?.logoURI
-        },
-        {
-            name: 'PSOL',
-            icon: tokens.find(x => x.symbol === "PSOL")?.logoURI
-        },
-        {
-            name: 'USDC',
-            icon: tokens.find(x => x.symbol === "USDC")?.logoURI
-        },
-        {
-            name: 'USDT',
-            icon: tokens.find(x => x.symbol === "USDT")?.logoURI
-        }
-    ]
-    return (
-        <Transition.Root show={visible} as={Fragment}>
-            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={close}>
-                <div
-                    className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0">
-                        <Dialog.Overlay
-                            className="fixed inset-0 bg-black backdrop-blur-sm bg-opacity-30 transition-opacity"/>
-                    </Transition.Child>
-                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen"
-                          aria-hidden="true">&#8203;</span>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        enterTo="opacity-100 translate-y-0 sm:scale-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-                        <div
-                            className="inline-block align-bottom rounded-xl text-left overflow-hidden shadow-strong transform transition-all sm:align-middle sm:max-w-sm sm:w-full">
-                            <div className="bg-[#231f38] text-gray-100 p-5 sm:py-7">
-                                <div className="flex gap-x-2 items-center mb-6 px-3">
-                                    <SearchIcon className={"w-6 text-gray-400"}/>
-                                    <input type={"search"}
-                                           spellCheck={"false"}
-                                           onChange={e => setValue(e.target.value)}
-                                           className={"w-full bg-transparent p-1 border-0 outline-0"}
-                                           placeholder={"Search by token or address"}/>
-                                </div>
-                                {/*<div className={"flex gap-x-2 justify-around mb-4"}>*/}
-                                {/*    {featuredTokens.map(token => (*/}
-                                {/*        <button className={"flex items-center text-xs gap-x-2 p-2 border border-white border-opacity-20 bg-white bg-opacity-5 rounded-lg hover:border-purple-2 hover:bg-purple-2 hover:bg-opacity-5"}>*/}
-                                {/*            <img className={"w-4"} src={token?.icon} alt={token.name} />*/}
-                                {/*            {token.name}*/}
-                                {/*        </button>*/}
-                                {/*    ))}*/}
-                                {/*</div>*/}
-                                <div
-                                    className={"max-h-[30rem] scrollbar-thin scrollbar-thumb-purple-2 scrollbar-thumb-rounded-full"}>
-                                    <div className="relative grid gap-y-2">
-                                        {filteredTokens.length > 0 ? filteredTokens.map((token) => (
-                                            <button
-                                                onClick={() => {
-                                                    if (mode == TokenChooserMode.Input) {
-                                                        setInput(new PublicKey(token.address))
-                                                    } else {
-                                                        setOutput(new PublicKey(token.address))
-                                                    }
-                                                    close()
-                                                }}
-                                                className="p-3 flex gap-x-4 items-center text-left rounded-lg focus:outline-none -focus:ring focus:ring-purple-2 hover:bg-white hover:bg-opacity-5 focus:bg-white focus:bg-opacity-5">
-                                                <img
-                                                    onError={({currentTarget}) => {
-                                                        currentTarget.onerror = null;
-                                                        currentTarget.src = 'https://raw.githubusercontent.com/parasol-labs-org/white-paper/main/logo.png'
-                                                    }}
-                                                    src={token.logoURI}
-                                                    className="w-6 h-6"
-                                                    alt={token.name}/>
-                                                <span
-                                                    className={"flex-1 w-0 max-w-xs font-medium truncate"}>{token.name}</span>
-                                                <span className="text-sm text-gray-400">
-                                                    {token.symbol}
-                                                </span>
-                                            </button>
-                                        )) : <span className={"text-gray-300 pt-6 pb-3"}>No results found.</span>}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Transition.Child>
-                </div>
-            </Dialog>
-        </Transition.Root>
+  const { visible, setVisible, mode, setInput, setOutput } = useTokenModal();
+  const [value, setValue] = useState("");
+  const [tokens, setTokens] = useState<Token[]>([]);
+  useEffect(() => {
+    fetch(TOKEN_LIST_URL["mainnet-beta"])
+      .then((response) => response.json())
+      .then((result) => setTokens(result));
+  }, []);
+  const filteredTokens = tokens.filter((token) => {
+    if (!value) return true;
+    if (
+      token.name.toLowerCase().includes(value.toLowerCase()) ||
+      token.symbol.toLowerCase().includes(value.toLowerCase()) ||
+      token.address.includes(value)
     )
-}
+      return true;
+  });
+  const close = () => {
+    setVisible(false);
+    setValue("");
+  };
+  let featuredTokens = [
+    {
+      name: "SOL",
+      icon: tokens.find((x) => x.symbol === "SOL")?.logoURI,
+    },
+    {
+      name: "PSOL",
+      icon: tokens.find((x) => x.symbol === "PSOL")?.logoURI,
+    },
+    {
+      name: "USDC",
+      icon: tokens.find((x) => x.symbol === "USDC")?.logoURI,
+    },
+    {
+      name: "USDT",
+      icon: tokens.find((x) => x.symbol === "USDT")?.logoURI,
+    },
+  ];
+  return (
+    <Transition.Root show={visible} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed z-10 inset-0 overflow-y-auto"
+        onClose={close}
+      >
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black backdrop-blur-sm bg-opacity-30 transition-opacity" />
+          </Transition.Child>
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block align-bottom rounded-xl text-left overflow-hidden shadow-strong transform transition-all sm:align-middle sm:max-w-sm sm:w-full">
+              <div className="bg-[#231f38] text-gray-100 p-5 sm:py-7">
+                <div className="flex gap-x-2 items-center mb-6 px-3">
+                  <SearchIcon className={"w-6 text-gray-400"} />
+                  <input
+                    type={"search"}
+                    spellCheck={"false"}
+                    onChange={(e) => setValue(e.target.value)}
+                    className={"w-full bg-transparent p-1 border-0 outline-0"}
+                    placeholder={"Search by token or address"}
+                  />
+                </div>
+                {/*<div className={"flex gap-x-2 justify-around mb-4"}>*/}
+                {/*    {featuredTokens.map(token => (*/}
+                {/*        <button className={"flex items-center text-xs gap-x-2 p-2 border border-white border-opacity-20 bg-white bg-opacity-5 rounded-lg hover:border-purple-2 hover:bg-purple-2 hover:bg-opacity-5"}>*/}
+                {/*            <img className={"w-4"} src={token?.icon} alt={token.name} />*/}
+                {/*            {token.name}*/}
+                {/*        </button>*/}
+                {/*    ))}*/}
+                {/*</div>*/}
+                <div
+                  className={
+                    "max-h-[30rem] scrollbar-thin scrollbar-thumb-purple-2 scrollbar-thumb-rounded-full"
+                  }
+                >
+                  <div className="relative grid gap-y-2">
+                    {filteredTokens.length > 0 ? (
+                      filteredTokens.map((token) => (
+                        <button
+                          key={token.address}
+                          onClick={() => {
+                            if (mode == TokenChooserMode.Input) {
+                              setInput(new PublicKey(token.address));
+                            } else {
+                              setOutput(new PublicKey(token.address));
+                            }
+                            close();
+                          }}
+                          className="p-3 flex gap-x-4 items-center text-left rounded-lg focus:outline-none -focus:ring focus:ring-purple-2 hover:bg-white hover:bg-opacity-5 focus:bg-white focus:bg-opacity-5"
+                        >
+                          <img
+                            onError={({ currentTarget }) => {
+                              currentTarget.onerror = null;
+                              currentTarget.src =
+                                "https://raw.githubusercontent.com/parasol-labs-org/white-paper/main/logo.png";
+                            }}
+                            src={token.logoURI}
+                            className="w-6 h-6"
+                            alt={token.name}
+                          />
+                          <span
+                            className={
+                              "flex-1 w-0 max-w-xs font-medium truncate"
+                            }
+                          >
+                            {token.name}
+                          </span>
+                          <span className="text-sm text-gray-400">
+                            {token.symbol}
+                          </span>
+                        </button>
+                      ))
+                    ) : (
+                      <span className={"text-gray-300 pt-6 pb-3"}>
+                        No results found.
+                      </span>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,85 +1,98 @@
-import '../styles/globals.css'
-import {WalletAdapterNetwork} from '@solana/wallet-adapter-base';
-import {ConnectionProvider, useConnection, useWallet, WalletProvider} from '@solana/wallet-adapter-react';
+import "../styles/globals.css";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
-    LedgerWalletAdapter,
-    PhantomWalletAdapter,
-    SlopeWalletAdapter,
-    SolflareWalletAdapter,
-    SolletExtensionWalletAdapter,
-    SolletWalletAdapter,
-    TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import {AppProps} from 'next/app';
-import React, {FC, useMemo} from 'react';
+  ConnectionProvider,
+  useConnection,
+  useWallet,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { AppProps } from "next/app";
+import React, { FC, useMemo } from "react";
 import Head from "next/head";
-import {JupiterProvider} from "@jup-ag/react-hook";
-import {TokenModalProvider} from "../components/token-chooser/TokenModalProvider";
+import { JupiterProvider } from "@jup-ag/react-hook";
+import { TokenModalProvider } from "../components/token-chooser/TokenModalProvider";
 import Header from "../components/header";
-import {WalletModalProvider} from "../components/wallet-connector";
+import { WalletModalProvider } from "../components/wallet-connector";
 import AnnounceBar from "../components/announce-bar";
 import Footer from "../components/footer";
-import SimpleReactLightbox from 'simple-react-lightbox'
+import SimpleReactLightbox from "simple-react-lightbox";
 
-const JupiterWrapper: React.FC = ({children}) => {
-    const {connection} = useConnection();
-    const wallet = useWallet();
-    return (
-        <JupiterProvider
-            cluster="mainnet-beta"
-            connection={connection}
-            userPublicKey={wallet.publicKey || undefined}
-            routeCacheDuration={5000}
-        >
-            {children}
-        </JupiterProvider>
-    );
+const JupiterWrapper: React.FC = ({ children }) => {
+  const { connection } = useConnection();
+  const wallet = useWallet();
+  return (
+    <JupiterProvider
+      // cluster="mainnet-beta"
+      cluster="mainnet-beta"
+      connection={connection}
+      userPublicKey={wallet.publicKey || undefined}
+      routeCacheDuration={5000}
+    >
+      {children}
+    </JupiterProvider>
+  );
 };
 
-const App: FC<AppProps> = ({Component, pageProps}) => {
-    const network = WalletAdapterNetwork.Mainnet;
-    const endpoint = "https://solana-api.projectserum.com" // useMemo(() => clusterApiUrl(network, true), [network]);
-    const wallets = useMemo(
-        () => [
-            new PhantomWalletAdapter(),
-            new SlopeWalletAdapter(),
-            new SolflareWalletAdapter({network}),
-            new TorusWalletAdapter(),
-            new LedgerWalletAdapter(),
-            new SolletWalletAdapter({network}),
-            new SolletExtensionWalletAdapter({network}),
-        ],
-        [network]
-    );
-    return <>
-        <Head>
-            <title>Parasol Finance ($PSOL) | Community Governed Launchpad on Solana</title>
-            <link rel="icon" href="/favicon.svg" type="image/svg"/>
-        </Head>
-        <body>
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const network = WalletAdapterNetwork.Mainnet;
+  // const network = WalletAdapterNetwork.Devnet;
+  const endpoint = "https://solana-api.projectserum.com"; // useMemo(() => clusterApiUrl(network, true), [network]);
+  // const endpoint = "https://api.devnet.solana.com";
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new SolletWalletAdapter({ network }),
+      new SolletExtensionWalletAdapter({ network }),
+    ],
+    [network]
+  );
+  return (
+    <>
+      <Head>
+        <title>
+          Parasol Finance ($PSOL) | Community Governed Launchpad on Solana
+        </title>
+        <link rel="icon" href="/favicon.svg" type="image/svg" />
+      </Head>
+      <div>
         <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider
-                wallets={wallets}
-                // onError={(messages) => alert(messages.error)}
-                autoConnect={true}>
-                <JupiterWrapper>
-                    <TokenModalProvider>
-                        <AnnounceBar/>
-                        <WalletModalProvider>
-                            <Header/>
-                            <main role={"main"}>
-                                <SimpleReactLightbox>
-                                    <Component {...pageProps} />
-                                </SimpleReactLightbox>
-                            </main>
-                            <Footer/>
-                        </WalletModalProvider>
-                    </TokenModalProvider>
-                </JupiterWrapper>
-            </WalletProvider>
+          <WalletProvider
+            wallets={wallets}
+            // onError={(messages) => alert(messages.error)}
+            autoConnect={true}
+          >
+            <JupiterWrapper>
+              <TokenModalProvider>
+                <AnnounceBar />
+                <WalletModalProvider>
+                  <Header />
+                  <main role={"main"}>
+                    <SimpleReactLightbox>
+                      <Component {...pageProps} />
+                    </SimpleReactLightbox>
+                  </main>
+                  <Footer />
+                </WalletModalProvider>
+              </TokenModalProvider>
+            </JupiterWrapper>
+          </WalletProvider>
         </ConnectionProvider>
-        </body>
-    </>;
+      </div>
+    </>
+  );
 };
 
 export default App;
