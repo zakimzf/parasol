f1d24e5e10ef511259e77d951ec33dfb626da674
Clint.Network <me@clint.network>
2022-03-04 23:06:21 +0200

    Add dynamic padding into `base-card`

--- src/components/cards/base-card.tsx
+++ src/components/cards/base-card.tsx
@@ -3,11 +3,12 @@ import React from "react";
 type CardBaseProps = {
 	children: React.ReactNode,
 	classes?: string,
-	hoverEffect?: boolean
+	hoverEffect?: boolean,
+  padding?: number
 };
 
-const BaseCard = ({children, hoverEffect, classes}: CardBaseProps) => {
-  const className = ["mx-4 lg:mx-0 flex-col bg-[#231f38] bg-opacity-50 shadow-xl shadow-half-strong border border-gray-800 rounded-lg overflow-hidden p-6"];
+const BaseCard = ({children, hoverEffect, classes, padding}: CardBaseProps) => {
+  const className = [`mx-4 lg:mx-0 flex-col bg-[#231f38] bg-opacity-50 shadow-xl shadow-half-strong border border-gray-800 rounded-lg overflow-hidden p-${padding}`];
   if (classes != null) {
     className.push(classes);
   }
