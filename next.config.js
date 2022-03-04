03ec194995bc007d582c95f5c75ab386f3de1da3
Clint.Network <me@clint.network>
2022-03-04 14:10:23 +0200

    Reindent files following new ESLint configuration

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	webpack: (config, { isServer }) => {
-		config.module.rules.push({
-			test: /\.md$/,
-			use: 'raw-loader',
-		});
-		if (!isServer) {
-			config.resolve.fallback.fs = false;
-		}
-		return config;
-	},
-	env: {
-		NETWORK: process.env.NETWORK || "dev",
-		PLATFORM_FEE_ADDRESS: process.env.PLATFORM_FEE_ADDRESS || "",
-	}
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+  env: {
+    NETWORK: process.env.NETWORK || "dev",
+    PLATFORM_FEE_ADDRESS: process.env.PLATFORM_FEE_ADDRESS || "",
+  }
 }
 
 module.exports = nextConfig
