91a2db59adff4612dad34da0b3fc48aec445f48d
ayoub <ayoubuv@gmail.com>
2022-03-26 14:25:59 +0100

    setup firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLhSg3k33UW0ywWzowz5PjqljpYHYX2Kg",
+  authDomain: "parasol-finance-21.firebaseapp.com",
+  projectId: "parasol-finance-21",
+  storageBucket: "parasol-finance-21.appspot.com",
+  messagingSenderId: "795707072474",
+  appId: "1:795707072474:web:c123c9dc6e35655b9eaae3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
