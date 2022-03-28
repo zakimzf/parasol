63722e64df9a12818e26bc71a83e950fbe4d6146
ayoub <ayoubuv@gmail.com>
2022-03-28 20:32:23 +0000

    get all projects

--- src/pages/api/projects/index.ts
+++ src/pages/api/projects/index.ts
@@ -1,13 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import type {NextApiRequest, NextApiResponse} from "next"
+import { db } from "../../../utils/firebase";
 
 type Data = {
   name: string
 }
 
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({name: "John Doe"})
+  const getProject = async()=>{
+    const {tokenAddress} = req.query;
+  
+    const data:any = await getAllProjects();
+    res.status(200).json(data)
+  }
+  getProject();
+}
+
+const getAllProjects = async() => {
+
+  const idosCollectionRef = collection(db, "idos");
+  const data = await getDocs(idosCollectionRef);
+  const projects = await data.docs.map((doc: any) => ({ ...doc.data(), id: doc.id }));
+  return projects;
+
 }
