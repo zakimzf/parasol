afc5b6779e3f0ff1d54728cc51d743b9fce4b52a
Clint.Network <me@clint.network>
2022-03-31 22:26:25 +0200

    Cleanup `functions.js`

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,36 +1,26 @@
 import { db } from "./firebase";
-import {
-  doc,
-  getDoc,
-} from "firebase/firestore";
+import { doc, getDoc, } from "firebase/firestore";
 
 export const getBase64 = (file, cb) => {
   let reader = new FileReader();
   reader.readAsDataURL(file);
-  reader.onload = function () {
-    cb(reader.result)
-  };
-  reader.onerror = function (error) {
-    console.log("Error: ", error);
-  };
+  reader.onload = () => cb(reader.result);
+  reader.onerror = error => console.log("Error: ", error);
 }
 
-
 export const validURL = (str) => {
-  const pattern = new RegExp("^(https?:\\/\\/)?"+ // protocol
-    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|"+ // domain name
-    "((\\d{1,3}\\.){3}\\d{1,3}))"+ // OR ip (v4) address
-    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*"+ // port and path
-    "(\\?[;&a-z\\d%_.~+=-]*)?"+ // query string
-    "(\\#[-a-z\\d_]*)?$","i"); // fragment locator
+  const pattern = new RegExp("^(https?:\\/\\/)?" + // protocol
+    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+    "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+    "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+    "(\\#[-a-z\\d_]*)?$", "i"); // fragment locator
   return !!pattern.test(str);
 }
 
-export const isTokenAddressExist = async (id)=>{
-  
+export const isTokenAddressExist = async (id) => {
   const docRef = doc(db, "idos", id);
   const docSnap = await getDoc(docRef);
-  
   return docSnap.exists();
 }
 
