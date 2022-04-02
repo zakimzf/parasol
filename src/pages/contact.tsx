b40e57bb3732131d67934eb885196e0f3caf5d68
ayoub <ayoubuv@gmail.com>
2022-04-02 11:58:02 +0000

    add hcaptcha

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,13 @@
 import Heading from "../components/heading";
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import { ChatAltIcon } from "@heroicons/react/solid";
 import { errClasses } from "../utils/functions";
 import { ExclamationCircleIcon } from "@heroicons/react/outline";
-import { addDoc, collection, doc, Timestamp } from "firebase/firestore";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
 import { db } from "../utils/firebase";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
 
 const Contact = () => {
   const contactCollectionRef = collection(db, "contacts");
@@ -38,8 +39,12 @@ const Contact = () => {
     setValues({ ...values, [name]: value });
   };
 
+  const [token, setToken] = useState<any>(null);
+  const captchaRef: any = useRef(null);
+
   const handleSubmit = async (e: any) => {
     e.preventDefault();
+
     setSendStatus(1);
 
     const _errors: any = [];
@@ -55,9 +60,14 @@ const Contact = () => {
     setErrors(_errors);
 
     if (Object.keys(_errors).length == 0) {
-      await addDoc(contactCollectionRef, values);
-      setSendStatus(2);
-    }else{
+      if (token) {
+        await addDoc(contactCollectionRef, values);
+        setSendStatus(2);
+      } else {
+        captchaRef.current.execute();
+        setSendStatus(0);
+      }
+    } else {
       setSendStatus(0);
     }
   };
@@ -212,7 +222,16 @@ const Contact = () => {
                   )}
                 </div>
               </div>
-              <div className={"col-span-2 flex justify-center gap-x-3 mt-6"}>
+
+              <div className={"col-span-2 flex justify-center gap-x-3"}>
+                <HCaptcha
+                  sitekey="c110b8ea-4a96-4587-8027-35c19245a3d2"
+                  onVerify={setToken}
+                  ref={captchaRef}
+                />
+              </div>
+
+              <div className={"col-span-2 flex justify-center gap-x-3 mt-3"}>
                 <button
                   type={"submit"}
                   className={
