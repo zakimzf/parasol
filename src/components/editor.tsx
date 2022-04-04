101e7066fcba0ca62b9a3fee10d4e1cde6582134
ayoub <ayoubuv@gmail.com>
2022-04-04 03:56:03 +0000

    add double quotes

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import InlineEditor from '@ckeditor/ckeditor5-build-inline';
+import React, { Component } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import InlineEditor from "@ckeditor/ckeditor5-build-inline";
 
-class Editor extends Component {
-    render() {
-        return (
-            <div>
-                <CKEditor
-                    editor={ InlineEditor }
-                    data="project description"
-                    onReady={ (editor:any) => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event:any, editor :any) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event:any, editor:any ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event:any, editor:any ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-            </div>
-        );
-    }
+const Editor = () => {
+    return (
+        <div className="border-2">
+            <CKEditor
+                editor={ InlineEditor }
+                data="project description"
+                onReady={ (editor:any) => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log( "Editor is ready to use!", editor );
+                } }
+                onChange={ ( event:any, editor :any) => {
+                    const data = editor.getData();
+                    console.log( { event, editor, data } );
+                } }
+                onBlur={ ( event:any, editor:any ) => {
+                    console.log( "Blur.", editor );
+                } }
+                onFocus={ ( event:any, editor:any ) => {
+                    console.log( "Focus.", editor );
+                } }
+            />
+        </div>
+    );
 }
 
 export default Editor;
