273e1d14211940e4b9dc332dd24485de31539ef2
ayoub <ayoubuv@gmail.com>
2022-04-04 04:05:51 +0000

    drop classic

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,31 +1,23 @@
-import React, { Component } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import InlineEditor from "@ckeditor/ckeditor5-build-inline";
 
 const Editor = () => {
-    return (
-        <div className="border-2">
-            <CKEditor
-                editor={ InlineEditor }
-                data="project description"
-                onReady={ (editor:any) => {
-                    // You can store the "editor" and use when it is needed.
-                    console.log( "Editor is ready to use!", editor );
-                } }
-                onChange={ ( event:any, editor :any) => {
-                    const data = editor.getData();
-                    console.log( { event, editor, data } );
-                } }
-                onBlur={ ( event:any, editor:any ) => {
-                    console.log( "Blur.", editor );
-                } }
-                onFocus={ ( event:any, editor:any ) => {
-                    console.log( "Focus.", editor );
-                } }
-            />
-        </div>
-    );
-}
+  return (
+    <div className="border-2">
+      <CKEditor
+        editor={InlineEditor}
+        data="project description"
+        onReady={(editor: any) => {
+          // You can store the "editor" and use when it is needed.
+          console.log("Editor is ready to use!", editor);
+        }}
+        onChange={(event: any, editor: any) => {
+          const data = editor.getData();
+          console.log({ event, editor, data });
+        }}
+      />
+    </div>
+  );
+};
 
 export default Editor;
