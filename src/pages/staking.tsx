6735122d65cb453d04bb7dceeffb17d7fcdf8003
soft-top-notch <soft.dev.96@gmail.com>
2022-04-04 18:05:01 -0700

    Fixed the image path issue

--- src/pages/staking.tsx
+++ src/pages/staking.tsx
@@ -188,7 +188,7 @@ const Staking = () => {
                     className="gap-x-2 py-2 px-2 rounded-lg flex items-center"
                   >
                     <img
-                      src={"/images/logos/parasol-logo-mark-reverse-rgb.svg"}
+                      src={"/assets/logos/parasol-logo-mark-reverse-rgb.svg"}
                       className="w-5 h-5 rounded-full"
                       alt={"PSOL"}
                     />
