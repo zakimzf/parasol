10ec284be8ae3421b4f7cde87f1f58434ba72359
Clint.Network <me@clint.network>
2022-04-09 23:10:03 +0200

    Reactivate lightbox

--- src/pages/projects/[tokenAddress]/index.tsx
+++ src/pages/projects/[tokenAddress]/index.tsx
@@ -7,6 +7,7 @@ import axios from "axios";
 import Link from "next/link";
 import NumberFormat from "react-number-format";
 import dynamic from "next/dynamic";
+import { SRLWrapper } from "simple-react-lightbox";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { ExternalLinkIcon, FireIcon } from "@heroicons/react/solid";
 import Disqus from "disqus-react";
@@ -52,102 +53,104 @@ const ProjectDetails = () => {
                   </p>
                 </div>
               </div>
-              <img src={ido.projectCover} className={"mb-6 rounded-lg cursor-pointer ease transition-transform duration-300 hover:scale-105"} alt={ido.name}/>
-              <Tab.Group>
-                <Tab.List className={"mb-3"}>
-                  <div className="border-b border-gray-500">
-                    <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-                      <Tab as={Fragment}>
-                        {({ selected }) => (
-                          <a
-                            href={"#details"}
-                            className={`${selected ? "border-purple-2 text-purple-2" : "border-transparent hover:text-purple-2 hover:border-purple-2"} whitespace-nowrap pt-2 pb-3 px-1 border-b-2 font-medium text-sm"`}
-                            aria-current={selected ? "page" : undefined}>
-                            Project Details
-                          </a>
-                        )}
-                      </Tab>
-                      <Tab as={Fragment}>
-                        {({ selected }) => (
-                          <a
-                            href={"#token"}
-                            className={`${selected ? "border-purple-2 text-purple-2" : "border-transparent hover:text-purple-2 hover:border-purple-2"} whitespace-nowrap pt-2 pb-3 px-1 border-b-2 font-medium text-sm"`}
-                            aria-current={selected ? "page" : undefined}>
-                            Token Details
-                          </a>
-                        )}
-                      </Tab>
-                      <Tab as={Fragment}>
-                        {({ selected }) => (
-                          <a
-                            href={"#wp"}
-                            className={`${selected ? "border-purple-2 text-purple-2" : "border-transparent hover:text-purple-2 hover:border-purple-2"} flex items-center gap-x-1 whitespace-nowrap pt-2 pb-3 px-1 border-b-2 font-medium text-sm"`}
-                            aria-current={selected ? "page" : undefined}>
-                            White Paper
-                          </a>
-                        )}
-                      </Tab>
-                      {ido.websiteUrl && <a
-                        href={ido.websiteUrl}
-                        target={"_blank"}
-                        className={"flex items-center gap-x-1 !ml-auto text-white px-3 pt-2 pb-3 font-medium text-sm"} rel="noreferrer">
-                        <ExternalLinkIcon className={"w-5"}/>
-                        Visit Website
-                      </a>}
-                    </nav>
-                  </div>
-                </Tab.List>
-                <Tab.Panels>
-                  <Tab.Panel>
-                    <div className={"prose markdown prose-lg prose-invert"}>
-                      <EditorJs
-                        content={ido.content || "{}"}
-                        isOwner={walletAddress && walletAddress == ido.publicKey || false}
-                        tokenAddress={tokenAddress}
-                      />
+              <SRLWrapper>
+                <img src={ido.projectCover} className={"mb-6 rounded-lg cursor-pointer ease transition-transform duration-300 hover:scale-105"} alt={ido.name}/>
+                <Tab.Group>
+                  <Tab.List className={"mb-3"}>
+                    <div className="border-b border-gray-500">
+                      <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                        <Tab as={Fragment}>
+                          {({ selected }) => (
+                            <a
+                              href={"#details"}
+                              className={`${selected ? "border-purple-2 text-purple-2" : "border-transparent hover:text-purple-2 hover:border-purple-2"} whitespace-nowrap pt-2 pb-3 px-1 border-b-2 font-medium text-sm"`}
+                              aria-current={selected ? "page" : undefined}>
+                              Project Details
+                            </a>
+                          )}
+                        </Tab>
+                        <Tab as={Fragment}>
+                          {({ selected }) => (
+                            <a
+                              href={"#token"}
+                              className={`${selected ? "border-purple-2 text-purple-2" : "border-transparent hover:text-purple-2 hover:border-purple-2"} whitespace-nowrap pt-2 pb-3 px-1 border-b-2 font-medium text-sm"`}
+                              aria-current={selected ? "page" : undefined}>
+                              Token Details
+                            </a>
+                          )}
+                        </Tab>
+                        <Tab as={Fragment}>
+                          {({ selected }) => (
+                            <a
+                              href={"#wp"}
+                              className={`${selected ? "border-purple-2 text-purple-2" : "border-transparent hover:text-purple-2 hover:border-purple-2"} flex items-center gap-x-1 whitespace-nowrap pt-2 pb-3 px-1 border-b-2 font-medium text-sm"`}
+                              aria-current={selected ? "page" : undefined}>
+                              White Paper
+                            </a>
+                          )}
+                        </Tab>
+                        {ido.websiteUrl && <a
+                          href={ido.websiteUrl}
+                          target={"_blank"}
+                          className={"flex items-center gap-x-1 !ml-auto text-white px-3 pt-2 pb-3 font-medium text-sm"} rel="noreferrer">
+                          <ExternalLinkIcon className={"w-5"}/>
+                          Visit Website
+                        </a>}
+                      </nav>
                     </div>
-                  </Tab.Panel>
-                  <Tab.Panel>
-                    <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 -rounded-md">
-                      <table className="min-w-full divide-y divide-gray-800 bg-[#231f38] bg-opacity-50 shadow-xl shadow-half-strong border border-gray-800 rounded-lg">
-                        <tbody className="divide-y divide-gray-800">
-                          <tr>
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Token Address</td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm">{tokenAddress}</td>
-                          </tr>
-                          <tr>
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Token Name</td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm">{ido.projectName}</td>
-                          </tr>
-                          <tr>
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Symbol</td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm">N/a</td>
-                          </tr>
-                          <tr>
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Decimals</td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm">7</td>
-                          </tr>
-                          <tr>
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Price (in USDT)</td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm">N/a</td>
-                          </tr>
-                          <tr>
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Market Cap (in USDT)</td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm">N/a</td>
-                          </tr>
-                          <tr>
-                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Volume 24h (in USDT)</td>
-                            <td className="whitespace-nowrap px-3 py-4 text-sm">N/a</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </Tab.Panel>
-                  <Tab.Panel>
-                    <iframe src={ido.whitepaperUrl + "#toolbar=0&navpanes=0"} className={"w-full my-3 min-h-screen border-none"}/>
-                  </Tab.Panel>
-                </Tab.Panels>
-              </Tab.Group>
+                  </Tab.List>
+                  <Tab.Panels>
+                    <Tab.Panel>
+                      <div className={"prose markdown prose-lg prose-invert"}>
+                        <EditorJs
+                          content={ido.content || "{}"}
+                          isOwner={walletAddress && walletAddress == ido.publicKey || false}
+                          tokenAddress={tokenAddress}
+                        />
+                      </div>
+                    </Tab.Panel>
+                    <Tab.Panel>
+                      <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 -rounded-md">
+                        <table className="min-w-full divide-y divide-gray-800 bg-[#231f38] bg-opacity-50 shadow-xl shadow-half-strong border border-gray-800 rounded-lg">
+                          <tbody className="divide-y divide-gray-800">
+                            <tr>
+                              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Token Address</td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm">{tokenAddress}</td>
+                            </tr>
+                            <tr>
+                              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Token Name</td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm">{ido.projectName}</td>
+                            </tr>
+                            <tr>
+                              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Symbol</td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm">N/a</td>
+                            </tr>
+                            <tr>
+                              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Decimals</td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm">7</td>
+                            </tr>
+                            <tr>
+                              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Price (in USDT)</td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm">N/a</td>
+                            </tr>
+                            <tr>
+                              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Market Cap (in USDT)</td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm">N/a</td>
+                            </tr>
+                            <tr>
+                              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6">Volume 24h (in USDT)</td>
+                              <td className="whitespace-nowrap px-3 py-4 text-sm">N/a</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </Tab.Panel>
+                    <Tab.Panel>
+                      <iframe src={ido.whitepaperUrl + "#toolbar=0&navpanes=0"} className={"w-full my-3 min-h-screen border-none"}/>
+                    </Tab.Panel>
+                  </Tab.Panels>
+                </Tab.Group>
+              </SRLWrapper>
               <div className={"mt-12"}>
                 <Disqus.DiscussionEmbed
                   shortname={"parasol-finance"}
