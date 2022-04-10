78ce24f7318c6d03ddb9706e79f36ff98397944e
ayoub <ayoubuv@gmail.com>
2022-04-10 10:02:04 +0000

    add save changes button

--- src/components/editorjs.tsx
+++ src/components/editorjs.tsx
@@ -52,34 +52,40 @@ const EditorJs: React.FC<props> = ({ tokenAddress, isOwner, content }) => {
         table: Table,
       },
       data: JSON.parse(content),
-      onChange: (api: any, event: any) => {
-        if (editor_) {
-          editor_
-            .save()
-            .then(async (outputData: any) => {
-              // Snackbar.show({
-              //   pos: "bottom-center",
-              //   text: "Content was successfully saved.",
-              //   backgroundColor: "#231f38",
-              //   showAction: false
-              // });
-              await updateDoc(idosCollectionRef, {
-                content: JSON.stringify(outputData),
-              });
-            })
-            .catch((error: any) => {
-              console.log("Saving failed: ", error);
-            });
-        }
-      },
-      onPaste: (event: any) => {
-        console.log(event, 44444444);
-      },
+      // onChange: (api: any, event: any) => {
+      // },
+      // onPaste: (event: any) => {
+      //   console.log(event);
+      // },
     });
     setEditor(editor_);
   };
 
-  return <div id={"editorjs"}/>;
+  useEffect(() => {
+    const btn = document.getElementById("saveEditor");
+    if (editor && btn) {
+      btn.addEventListener("click", () => {
+        editor
+          .save()
+          .then(async (outputData: any) => {
+            await updateDoc(idosCollectionRef, {
+              content: JSON.stringify(outputData),
+            });
+            // Snackbar.show({
+            //   pos: "bottom-center",
+            //   text: "Content was successfully saved.",
+            //   backgroundColor: "#231f38",
+            //   showAction: false
+            // });
+          })
+          .catch((error: any) => {
+            // console.log("Saving failed: ", error);
+          });
+      });
+    }
+  }, [editor]);
+
+  return <div id={"editorjs"} />;
 };
 
 export default EditorJs;
