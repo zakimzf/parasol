2c7161609a861f8c7320fe6a0f5e29f8259bdeab
Clint.Network <me@clint.network>
2022-04-14 17:59:18 +0200

    Refactor `Brands` section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from "next"
 import Hero from "../components/slices/hero";
 import Projects from "../components/slices/projects";
 import Layout from "../components/layout";
+import Brands from "../components/slices/brands";
 
 const Home: NextPage = () =>
   <Layout>
     <Hero />
     <Projects />
+    <Brands/>
   </Layout>
 
 export default Home;
