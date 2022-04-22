e271c89b018ee3ccf46a05dbb0dcf7a864b84662
Clint.Network <me@clint.network>
2022-04-22 23:39:37 +0200

    Add Status + Countdown + Reminder button

--- src/components/cards/project-card.tsx
+++ src/components/cards/project-card.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 
 import Card from "../card";
 import { BadgeCheckIcon } from "@heroicons/react/solid";
+import { BellIcon, CollectionIcon } from "@heroicons/react/outline";
+import Countdown from "react-countdown";
 
 type ProjectDetails = {
   id: String;
@@ -11,9 +13,19 @@ type ProjectDetails = {
   name: String;
   description: String;
   price?: number
+  status: string,
   startTime: any;
   endTime: any;
 };
+
+type CountdownProps = {
+  days: number,
+  hours: number,
+  minutes: number,
+  seconds: number
+}
+
+const countdownRenderer = ({ days, hours, minutes, seconds }: CountdownProps) => <span>{days} Days - {hours}h {minutes}m {seconds}s</span>
 
 const ProjectCard = ({
   id,
@@ -23,6 +35,7 @@ const ProjectCard = ({
   logo,
   cover,
   price,
+  status,
   startTime,
   endTime
 }: ProjectDetails) => {
@@ -30,18 +43,25 @@ const ProjectCard = ({
   const endTime_ = new Date(endTime).toISOString().slice(0, 10);
   return (
     <Card>
-      {cover && (
-        <Link href={`/projects/${id}`}>
-          <a style={{ position: "relative" }}>
-            <img
-              className="w-full rounded-t-lg"
-              src={cover}
-              alt={`${id}-cover`}
-            />
-          </a>
-        </Link>
-      )}
-      {/*<label className="absolute z-10 top-3 right-3 p-2 bg-opacity-100 text-xs uppercase font-medium rounded-md bg-purple-1">Whitelist Opened</label>*/}
+      <div className={"relative"}>
+        <label className="absolute z-10 top-3 right-3 p-2 bg-opacity-70 text-xs uppercase font-medium rounded-md bg-purple-1">
+          Status: {status}
+        </label>
+        <div className={"w-full flex justify-center py-1 font-bold items-center absolute bg-white bg-opacity-10 z-10 bottom-0"}>
+          <Countdown date={startTime} renderer={countdownRenderer} />
+        </div>
+        {cover && (
+          <Link href={`/projects/${id}`}>
+            <a style={{ position: "relative" }}>
+              <img
+                className="w-full aspect-video rounded-t-lg"
+                src={cover}
+                alt={`${id}-cover`}
+              />
+            </a>
+          </Link>
+        )}
+      </div>
       <div className={`flex flex-col px-6 ${logo ? "pt-16" : "pt-6"} pb-6`}>
         {logo && (
           <img
@@ -56,11 +76,11 @@ const ProjectCard = ({
           </Link>
           {featured && <BadgeCheckIcon className={"h-7 text-purple-2"} />}
         </h2>
-        <p className="text text-gray-300 font-extralight">{description}</p>
+        <p className="text text-gray-300 mb-3 line-clamp-3">{description}</p>
         <div className="flex-col space-y-3 mt-3 mb-8">
           <div className="flex font-medium items-center text-gray-300 gap-x-3">
             <div className="flex items-center gap-x-1">
-              IDO Start Date
+                IDO Start Date
             </div>
             <span className="flex-1 h-1 border-b border-dashed border-gray-400" />
             <span>{startTime_}</span>
@@ -72,13 +92,21 @@ const ProjectCard = ({
           </div>
         </div>
         <div className="flex gap-x-3">
-          <Link href={`/projects/${id}/participate`} passHref>
-            <button className="button py-3 flex-1 text-base mb-3 whitespace-nowrap">
-              Participate in IDO
+          {startTime >= Date.now() ? (
+            <button className="button py-3 flex-1 gap-x-1 text-base whitespace-nowrap">
+              <BellIcon className={"w-5 h-5"} />
+                  Set a Reminder
             </button>
-          </Link>
+          ) : (
+            <Link href={`/projects/${id}/participate`} passHref>
+              <button className="button py-3 flex-1 text-base whitespace-nowrap">
+                <CollectionIcon className={"w-5 h-5"} />
+                  Participate in IDO
+              </button>
+            </Link>
+          )}
           <Link href={`/projects/${id}`} passHref>
-            <button className="button py-3 flex-1 text-base mb-3">More Info</button>
+            <button className="button py-3 flex-1 text-base">More Info</button>
           </Link>
         </div>
       </div>
