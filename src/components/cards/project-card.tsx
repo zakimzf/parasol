394c458e509d0c63870287d8245ff7a818479493
Clint.Network <me@clint.network>
2022-04-23 19:15:03 +0200

    Only display countdown for upcoming IDOs

--- src/components/cards/project-card.tsx
+++ src/components/cards/project-card.tsx
@@ -25,7 +25,8 @@ type CountdownProps = {
   seconds: number
 }
 
-const countdownRenderer = ({ days, hours, minutes, seconds }: CountdownProps) => <span>{days} Days - {hours}h {minutes}m {seconds}s</span>
+const countdownRenderer = ({ days, hours, minutes, seconds }: CountdownProps) =>
+  <span>{days} Days - {hours}h {minutes}m {seconds}s</span>
 
 const ProjectCard = ({
   id,
@@ -46,11 +47,13 @@ const ProjectCard = ({
       {cover && (
         <div className={"relative"}>
           <label className="absolute z-10 top-3 right-3 p-2 bg-opacity-70 text-xs uppercase font-medium rounded-md bg-purple-1">
-              IDO Status: {status}
+            IDO Status: {status}
           </label>
-          <div className={"w-full flex justify-center py-1 font-bold items-center absolute bg-white bg-opacity-10 z-10 bottom-0"}>
-            <Countdown date={startTime} renderer={countdownRenderer} />
-          </div>
+          {startTime >= Date.now() ? (
+            <div className={"w-full flex justify-center py-1 font-bold items-center absolute bg-white bg-opacity-10 z-10 bottom-0"}>
+              <Countdown date={startTime} renderer={countdownRenderer}/>
+            </div>
+          ) : ""}
           <Link href={`/projects/${id}`}>
             <a style={{ position: "relative" }}>
               <img
@@ -74,34 +77,32 @@ const ProjectCard = ({
           <Link href={`/projects/${id}`}>
             <a>{name}</a>
           </Link>
-          {featured && <BadgeCheckIcon className={"h-7 text-purple-2"} />}
+          {featured && <BadgeCheckIcon className={"h-7 text-purple-2"}/>}
         </h2>
         <p className="text text-gray-300 mb-3 line-clamp-3">{description}</p>
         <div className="flex-col space-y-3 mt-3 mb-8">
           <div className="flex font-medium items-center text-gray-300 gap-x-3">
-            <div className="flex items-center gap-x-1">
-                IDO Start Date
-            </div>
-            <span className="flex-1 h-1 border-b border-dashed border-gray-400" />
+            <div className="flex items-center gap-x-1">IDO Start Date</div>
+            <span className="flex-1 h-1 border-b border-dashed border-gray-400"/>
             <span>{startTime_}</span>
           </div>
           <div className="flex font-medium items-center text-gray-300 gap-x-3">
             <span>IDO End Date</span>
-            <span className="flex-1 h-1 border-b border-dashed border-gray-400" />
+            <span className="flex-1 h-1 border-b border-dashed border-gray-400"/>
             <span>{endTime_}</span>
           </div>
         </div>
         <div className="flex gap-x-3">
           {startTime >= Date.now() ? (
             <button className="button py-3 flex-1 gap-x-1 text-base whitespace-nowrap">
-              <BellIcon className={"w-5 h-5"} />
-                  Set a Reminder
+              <BellIcon className={"w-5 h-5"}/>
+              Set a Reminder
             </button>
           ) : (
             <Link href={`/projects/${id}/participate`} passHref>
               <button className="button py-3 flex-1 text-base whitespace-nowrap">
-                <CollectionIcon className={"w-5 h-5"} />
-                  Participate in IDO
+                <CollectionIcon className={"w-5 h-5"}/>
+                Participate in IDO
               </button>
             </Link>
           )}
