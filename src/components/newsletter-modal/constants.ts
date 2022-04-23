220d2d62e1305799a821ad195da8cd26adfd7d8c
Clint.Network <me@clint.network>
2022-04-23 16:22:27 +0200

    Cleanup

--- src/components/newsletter-modal/constants.ts
+++
@@ -1,4 +0,0 @@
-import { useJupiter } from "@jup-ag/react-hook";
-
-export interface Newsletter {}
-export type UseJupiterProps = Parameters<typeof useJupiter>[0];
--- src/components/newsletter-modal/useNewsletterModal.tsx
+++ src/components/newsletter-modal/useNewsletterModal.tsx
@@ -1,10 +1,4 @@
 import { createContext, useContext } from "react";
-import { PublicKey } from "@solana/web3.js";
-
-export enum NewsletterChooserMode {
-  Input,
-  Output
-}
 
 export interface NewsletterModalContextState {
   reminder: boolean;
