c015fb658df1510722bd83418492654b4e26b33b
Clint.Network <me@clint.network>
2022-04-24 13:12:35 +0200

    Create base page

---
+++ src/pages/tools/spl-creator.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Heading from "../../components/heading";
+
+const SplCreator: NextPage = () => {
+  return (
+    <>
+      <Heading tagline={"Parasol Finance Toolbox"} title={"SPL Token Creator"} description={"The perfect place to create a Solana token."} />
+    </>
+  )
+}
+
+export default SplCreator;
