9ef18a260cf2ba7301e5b3e49251cc9ee1384578
Clint.Network <me@clint.network>
2022-04-30 00:15:13 +0200

    Add overlay when sales is over

--- src/pages/projects/[projectPubKey]/index.tsx
+++ src/pages/projects/[projectPubKey]/index.tsx
@@ -303,6 +303,13 @@ const ProjectDetails = () => {
                   <div className="sticky flex flex-col gap-y-6 top-20">
                     <Card>
                       <div className={"flex flex-col gap-y-6 p-6"}>
+                        {ido.saleTotalAmount >= ido.hardCap && (
+                          <div className={"flex z-10 flex-col absolute justify-center items-center inset-0 backdrop-blur-sm"}>
+                            {/*<CheckIcon className={"w-20 h-20 mb-3 rounded-full bg-green-400 bg-opacity-20 p-3 text-green-600"} />*/}
+                            <h2 className={"text-3xl font-bold"}>Sale is Over</h2>
+                            <p className={"text-lg font-medium"}>The sale of {ido.name} is completed.</p>
+                          </div>
+                        )}
                         <div className={"flex justify-between items-center"}>
                           <h2 className="flex gap-x-2 items-center text-2xl font-bold">
                             {ido.name}
