5b376379099ab1fdcccc43816eb13228198c9500
Clint.Network <me@clint.network>
2022-05-04 22:36:08 +0200

    Add github link in menu

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -179,15 +179,30 @@ const Footer = () => (
           Copyright &copy; {new Date().getFullYear()} Parasol
           Finance. All rights reserved.
         </p>
-        <p className="flex-1 lg:text-right text-center">
-          Build with
-          <svg className="w-5 h-5 text-purple-2 mb-1 inline mx-2" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd"
-              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-              clipRule="evenodd"/>
-          </svg>
-          in Europe - Crafted by <a href={"https://twitter.com/thisisclint21"} className={"font-medium"} target={"_blank"} rel={"noreferrer"}>Clint ⚡️</a>
+        <p className="flex flex-1 items-center gap-x-3 justify-end lg:text-right text-center">
+          <div>
+            Build with
+            <svg className="w-5 h-5 text-purple-2 mb-1 inline mx-2" fill="currentColor" viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd"
+                d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                clipRule="evenodd"/>
+            </svg>
+            in Europe
+          </div>
+          <span>|</span>
+          <a className={"inline-flex justify-center items-center gap-x-1"} href={"https://github.com/parasol-finance/application"} target={"_blank"} rel={"noreferrer"}>
+            <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <path fillRule="evenodd"
+                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                clipRule="evenodd"/>
+            </svg>
+            Official GitHub
+          </a>
+          <span>|</span>
+          <div>
+            Crafted by <a href={"https://twitter.com/thisisclint21"} className={"font-medium"} target={"_blank"} rel={"noreferrer"}>Clint ⚡️</a>
+          </div>
         </p>
       </div>
     </div>
