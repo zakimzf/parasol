c3abfc762d2fa9fa0133d95d1131da710189325e
Holmes <haodev007@gmail.com>
2022-05-06 02:10:07 +0900

    complete ido claim

--- src/pages/projects/claim.tsx
+++ src/pages/projects/claim.tsx
@@ -2,6 +2,9 @@ import React, { Fragment, useContext, useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 
+import { PublicKey } from "@solana/web3.js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+
 import { Listbox, Transition } from "@headlessui/react";
 import {
   CheckIcon,
@@ -10,25 +13,68 @@ import {
   InformationCircleIcon,
 } from "@heroicons/react/outline";
 
+import { Loading } from "components";
+
 import Container from "components/container";
 import Layout from "components/layout";
 import { NftContext } from "context/NftContext";
-
-const idos = [
-  {
-    id: "lol",
-    name: "Secretum",
-    description:
-      "Revolutionary Secure Messaging & Trading Dapp powered by Solana.",
-  },
-];
+import { globalErrorHandle, notification } from "utils/functions";
 
 const TokenClaiming = () => {
-  const { nfts } = useContext(NftContext);
+  const { sendTransaction } = useWallet();
+  const { connection } = useConnection();
+
+  const [selectedNft, setSelectedNft] = useState<any>();
+  const [participatedReceipts, setParticipatedReceipt] = useState<any>();
+  const { nfts, wallet, user } = useContext(NftContext);
+
+  let mintAddress: PublicKey;
 
   useEffect(() => setSelectedNft(nfts[0]), [nfts]);
 
-  const [selectedNft, setSelectedNft] = useState<any>();
+  useEffect(() => {
+    if (wallet.connected) {
+      getParticipateReceiptsInfo();
+    }
+  }, [selectedNft, wallet]);
+
+  const claimToken = async (participatedReceipt: any) => {
+    mintAddress = new PublicKey(selectedNft.mint);
+
+    try {
+      const project = new PublicKey(participatedReceipt.project);
+
+      const tx = await user.claimFor(mintAddress, project);
+      let signature = await sendTransaction(tx, connection);
+      await connection.confirmTransaction(signature, "confirmed");
+      notification(
+        "success",
+        "Successfully claimed the token.",
+        "Transaction Success"
+      );
+      getParticipateReceiptsInfo();
+    }
+    catch (error: any) {
+      console.log(error, "error");
+      globalErrorHandle(error);
+    }
+  };
+
+  const getParticipateReceiptsInfo = async () => {
+    if (selectedNft) {
+      mintAddress = new PublicKey(selectedNft.mint);
+      try {
+        const participatedReceipt_ = await user.getParticipateReceipts(
+          mintAddress
+        );
+        setParticipatedReceipt(participatedReceipt_);
+      }
+      catch (error: any) {
+        console.log(error, "error_");
+      }
+    }
+  };
+
   return (
     <>
       <Head>
@@ -58,7 +104,7 @@ const TokenClaiming = () => {
               </div>
             </div>
             <div className="relative mt-5 flex items-center justify-center gap-x-2 md:justify-end">
-              {selectedNft && (
+              {selectedNft && wallet.connected && (
                 <Listbox value={selectedNft} onChange={setSelectedNft}>
                   <Listbox.Button className="relative flex cursor-pointer items-center gap-x-3 rounded-lg border-2 border-purple-2 bg-purple-2 bg-opacity-5 p-3 shadow-md focus:outline-none">
                     <div className="flex items-center">
@@ -129,75 +175,96 @@ const TokenClaiming = () => {
       <Layout>
         <section>
           <Container>
-            <ul
-              role="list"
-              className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
-            >
-              {idos.map((ido) => (
-                <li key={ido.id} className="card col-span-1">
-                  <div className="flex w-full items-center justify-between space-x-6 p-6">
-                    <div className="flex-1 truncate">
-                      <div className="flex items-center space-x-3">
-                        <h3 className="truncate font-medium">{ido.name}</h3>
-                        <span className="inline-block flex-shrink-0 rounded-full bg-purple-2 px-2 py-0.5 text-xs font-medium">
-                          Finished
-                        </span>
-                      </div>
-                      <p className="mt-1 truncate text-sm">{ido.description}</p>
-                      <div className="mt-3 grid grid-cols-2 gap-x-3 gap-y-6">
-                        <div>
-                          <div className="flex items-center gap-x-3 text-sm font-medium">
-                            <span>Purchased</span>
-                            <span className="h-1 flex-1 border-b border-dashed border-gray-400" />
-                            <span className="flex items-center gap-x-1">
-                              50$
+            {participatedReceipts ? (
+              <ul
+                role="list"
+                className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+              >
+                {participatedReceipts.map(
+                  (participatedReceipt: any, index: number) => (
+                    <li
+                      key={`participatedReceipt_${index}`}
+                      className="card col-span-1"
+                    >
+                      <div className="flex w-full items-center justify-between space-x-6 p-6">
+                        <div className="flex-1 truncate">
+                          <div className="flex items-center space-x-3">
+                            <h3 className="truncate font-medium">
+                              {participatedReceipt.name}
+                            </h3>
+                            <span className="inline-block flex-shrink-0 rounded-full bg-purple-2 px-2 py-0.5 text-xs font-medium">
+                              {participatedReceipt.status}
                             </span>
                           </div>
-                          <div className="flex items-center gap-x-3 text-sm font-medium">
-                            <span>Released</span>
-                            <span className="h-1 flex-1 border-b border-dashed border-gray-400" />
-                            <span className="flex items-center gap-x-1">
-                              25%
-                            </span>
+                          <p className="mt-1 truncate text-sm">
+                            {participatedReceipt.description}
+                          </p>
+                          <div className="mt-3 grid grid-cols-2 gap-x-3 gap-y-6">
+                            <div>
+                              <div className="flex items-center gap-x-3 text-sm font-medium">
+                                <span>Purchased</span>
+                                <span className="h-1 flex-1 border-b border-dashed border-gray-400" />
+                                <span className="flex items-center gap-x-1">
+                                  {participatedReceipt.purchaseAmount.toLocaleString()}
+                                  ${participatedReceipt.symbol}
+                                </span>
+                              </div>
+                              <div className="flex items-center gap-x-3 text-sm font-medium">
+                                <span>Released</span>
+                                <span className="h-1 flex-1 border-b border-dashed border-gray-400" />
+                                <span className="flex items-center gap-x-1">
+                                  {participatedReceipt.allowanceFeeBasisPoints *
+                                    100}
+                                  %
+                                </span>
+                              </div>
+                            </div>
+                            <div>
+                              <div className="flex items-center gap-x-3 text-sm font-medium">
+                                <span>Claimed</span>
+                                <span className="h-1 flex-1 border-b border-dashed border-gray-400" />
+                                <span className="flex items-center gap-x-1">
+                                  {participatedReceipt.claimedAmount.toLocaleString()}
+                                  ${participatedReceipt.symbol}
+                                </span>
+                              </div>
+                              <div className="flex items-center gap-x-3 text-sm font-medium">
+                                <span>Claimable</span>
+                                <span className="h-1 flex-1 border-b border-dashed border-gray-400" />
+                                <span className="flex items-center gap-x-1">
+                                  {participatedReceipt.claimableAmount.toLocaleString()}
+                                  ${participatedReceipt.symbol}
+                                </span>
+                              </div>
+                            </div>
                           </div>
                         </div>
-                        <div>
-                          <div className="flex items-center gap-x-3 text-sm font-medium">
-                            <span>Claimed</span>
-                            <span className="h-1 flex-1 border-b border-dashed border-gray-400" />
-                            <span className="flex items-center gap-x-1">
-                              25$
-                            </span>
-                          </div>
-                          <div className="flex items-center gap-x-3 text-sm font-medium">
-                            <span>Claimable</span>
-                            <span className="h-1 flex-1 border-b border-dashed border-gray-400" />
-                            <span className="flex items-center gap-x-1">
-                              30$
-                            </span>
-                          </div>
-                        </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className="flex divide-x divide-gray-800 border-t border-gray-800">
-                    <button className="flex flex-1 items-center justify-center gap-x-2 py-4">
-                      <HandIcon className="h-5 w-5" aria-hidden="true" />
-                      <span>Claim Tokens</span>
-                    </button>
-                    <Link href="/projects/x">
-                      <a className="flex flex-1 items-center justify-center gap-x-2 py-4">
-                        <InformationCircleIcon
-                          className="h-5 w-5"
-                          aria-hidden="true"
-                        />
-                        <span>IDO Details</span>
-                      </a>
-                    </Link>
-                  </div>
-                </li>
-              ))}
-            </ul>
+                      <div className="flex divide-x divide-gray-800 border-t border-gray-800">
+                        <button
+                          onClick={() => claimToken(participatedReceipt)}
+                          className="flex flex-1 items-center justify-center gap-x-2 py-4"
+                        >
+                          <HandIcon className="h-5 w-5" aria-hidden="true" />
+                          <span>Claim Tokens</span>
+                        </button>
+                        <Link href="/projects/x">
+                          <a className="flex flex-1 items-center justify-center gap-x-2 py-4">
+                            <InformationCircleIcon
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                            />
+                            <span>IDO Details</span>
+                          </a>
+                        </Link>
+                      </div>
+                    </li>
+                  )
+                )}
+              </ul>
+            ) : (
+              wallet.connected && <Loading />
+            )}
           </Container>
         </section>
       </Layout>
