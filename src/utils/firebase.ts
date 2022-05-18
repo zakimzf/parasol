b42767de6b12a807052c86efd98dd43a75314320
Holmes <haodev007@gmail.com>
2022-05-19 02:56:30 +0900

    server runtime config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,14 +1,18 @@
+import getConfig from "next/config"
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+const { serverRuntimeConfig } = getConfig();
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: serverRuntimeConfig.apiKey,
+  authDomain: serverRuntimeConfig.authDomain,
+  projectId: serverRuntimeConfig.projectId,
+  storageBucket: serverRuntimeConfig.storageBucket,
+  messagingSenderId: serverRuntimeConfig.messagingSenderId,
+  appId: serverRuntimeConfig.appId,
 };
 
 const app = initializeApp(firebaseConfig);
